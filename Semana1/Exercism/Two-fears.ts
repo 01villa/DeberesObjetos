9da598f3166d6aa3990a85a868f8b63f7722f0ae
01villa <146044992+01villa@users.noreply.github.com>
2024-05-13 23:28:35 -0500

    Update Two-fears.ts

--- Semana1/Exercism/Two-fears.ts
+++ Semana1/Exercism/Two-fears.ts
@@ -1,3 +1,7 @@
 export function twoFer(name: string = 'you'): string {
     return `One for ${name}, one for me.`;
   }
+
+  twoFer() // one for you  one for me
+twoFer("Pepe") // one for pepe one for me
+twoFer("") // one for  ,one for me 
